ef0cfd347b4c9d330e27f005ec41751e3bc2dce7
Shama <shamaanjum09@outlook.com>
2017-11-29 18:52:39 -0800

    basic html and pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+// generate a random number between 19 - 120.
+// crytals should have random value generated between  1 - 12.
+// total score, wins and loses are set to zero.
+// As user clicks on crystals total score gets updated.
+// the total score adds up with crystal values until it equals or exceeds random number 
+// if total score equals random number then user wins i.e number of wins is incremented by 1 and prints "You won!"
+// if total score exceeds random number then user loses i.e number of loses is incremented by 1 and prints "You lose!"
+// if user wins or loses game restarts i.e total score and score counter are reset to zero, new random number is generated and all the crystals will have four new hidden values.
+// The app should show the number of games the player wins and loses.
+//
