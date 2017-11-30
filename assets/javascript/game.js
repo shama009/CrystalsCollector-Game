8505df958912242df2b9ab7d695128e2f75622a0
Shama <shamaanjum09@outlook.com>
2017-11-29 21:50:31 -0800

    generate random number for game and for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,49 @@
+// variables
+var randomNumber=0;
+var crystal1=0;
+var crystal2=0;
+var crystal3=0;
+var crystal4=0;
+var wins=0;
+var loses=0;
+var totalScore=0;
+var scoreCounter=0;
+// function to generate a random number for a range.
+function generateRandomNum(min,max){
+    
+    var random= Math.floor(Math.random() * ((max-min)+1) + min);
+    return random;
+}
 
-// generate a random number between 19 - 120.
+$(document).ready(function(){
+    // generate a random number between 19 - 120.
+randomNumber=generateRandomNum(19,120);
+    // display random number generated
+$("#random-number").text(randomNumber);
+    // crytals should have random value generated between  1 - 12.
+crystal1=generateRandomNum(1,12);
+
+crystal2=generateRandomNum(1,12);
+crystal3=generateRandomNum(1,12);
+crystal4=generateRandomNum(1,12);
+});
+
+
+
+
+function reset(){
+    // generate a random number between 19 - 120.
+randomNumber=generateRandomNum(19,120);
 // crytals should have random value generated between  1 - 12.
-// total score, wins and loses are set to zero.
+crystal1=generateRandomNum(1,12);
+crystal2=generateRandomNum(1,12);
+crystal3=generateRandomNum(1,12);
+crystal4=generateRandomNum(1,12);
+// total score and score counter are set to zero.
+var totalScore=0;
+var scoreCounter=0;
+}
+// Assign random value generated to each crystal image
 // As user clicks on crystals total score gets updated.
 // the total score adds up with crystal values until it equals or exceeds random number 
 // if total score equals random number then user wins i.e number of wins is incremented by 1 and prints "You won!"
